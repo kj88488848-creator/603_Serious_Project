1d4388b4c9ff668bcd89ca3d4d437e50852ed391
ThorFrostick <145503610+ThorFrostick@users.noreply.github.com>
2026-02-08 10:50:23 -0500

    Added means to Reject policies

--- Assets/Scripts/CheckPolicyCollision.cs
+++ Assets/Scripts/CheckPolicyCollision.cs
@@ -8,14 +8,21 @@ public class CheckPolicyCollision : MonoBehaviour
 
     //Additionally, we need the RectTransform of this zone.
     private RectTransform approvedZone;
+
+    //Next, do the same for the Rejected Zone.
+    private GameObject rejected;
+    private RectTransform rejectedZone;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //Get the Approved element present in our scene.
+        //Get the Approved element present in our scene and it's RectTransform.
         approved = GameObject.FindGameObjectWithTag("Approved");
-
         approvedZone = approved.GetComponent<RectTransform>();
+
+        //Get the Rejected element and RectTransform.
+        rejected = GameObject.FindGameObjectWithTag("Rejected");
+        rejectedZone = rejected.GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
@@ -28,6 +35,13 @@ public class CheckPolicyCollision : MonoBehaviour
             if(ZoneCollision.IsOverlapping(DraggableElement.pos, approvedZone))
             {
                 Debug.Log("Approved");
+                DraggableElement.pos.anchoredPosition = Vector3.zero;
+            }
+
+            //Check if this UI element is overlapping with the Rejected zone.
+            if(ZoneCollision.IsOverlapping(DraggableElement.pos, rejectedZone))
+            {
+                Debug.Log("Rejected");
                 DraggableElement.pos.anchoredPosition = Vector3.zero;
             }
         }
